6046691c1efb213b059ca622a3756ff74189d8a6
MilkDevelopment <42803760+MilkDevelopment@users.noreply.github.com>
2022-11-14 20:47:37 +0100

    first commit

--- app.py
+++ app.py
@@ -7,14 +7,26 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+def generate_prompt(product):
+    return "Write a creative ad for the following product to run on Facebook aimed at businesses:\n\nProduct:" + str(product)
+
+
+def generate_proofread(proofread):
+    return "Correct this to standard English:\n\n" + str(proofread)
+
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        product = request.form["product"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(product),
+            temperature=0.5,
+            max_tokens=90,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +34,20 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+@app.route("/proofread", methods=("GET", "POST"))
+def proofread():
+    if request.method == "POST":
+        proofread = request.form["text"]
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=generate_proofread(proofread),
+            temperature=0,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
